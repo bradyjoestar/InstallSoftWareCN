863fe097adc6241ae014c97a7c80c5a6ff314225
bradyjoestar <bradyjoestar@gmail.com>
2017-06-16 20:33:37 +0800

    Create installJava_8.sh

---
+++ installJava_8.sh
@@ -0,0 +1,7 @@
+apt-get install python-software-properties -y
+apt-get install software-properties-common -y
+sudo add-apt-repository ppa:webupd8team/java
+
+
+apt-get update
+sudo apt-get install oracle-java8-installer
