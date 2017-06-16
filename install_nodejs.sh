5f00e72162696f9880b48da6af0833aa7f552fb5
bradyjoestar <bradyjoestar@gmail.com>
2017-06-16 20:42:39 +0800

    Update install_nodejs.sh

--- install_nodejs.sh
+++ install_nodejs.sh
@@ -1,7 +1,7 @@
 apt-get install python gcc make g++ 
 wget https://npm.taobao.org/mirrors/node/v6.9.1/node-v6.9.1.tar.gz
 tar zxvf node-v6.9.1.tar.gz 
-cd node-v6.9.1.tar.gz/ 
+cd node-v6.9.1.tar.gz
 ./configure 
 sudo make install 
 
